0d38811a9ccc57f5afa8a6d6d222023f8af15f15
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-02 20:28:59 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
   def clickbaity?
     #binding.pry
-    if self.title 
+    if self.title
       errors.add(:title, "must be clickbait-y") unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess") || title.include?("Top [0-9]")
     end
   end
